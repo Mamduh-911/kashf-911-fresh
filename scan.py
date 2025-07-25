0a2e67f96d40123a3a25bd5dace7135097006b6e
Mamduh-911 <132438162+Mamduh-911@users.noreply.github.com>
2025-07-26 00:37:50 +0300

    Create  scan.py

---
+++ scan.py
@@ -0,0 +1,17 @@
+# scan.py
+
+def run_scan(url):
+    return [
+        {
+            "type": "XSS",
+            "url": url,
+            "description": "تم اكتشاف ثغرة XSS في المعلمة `q`",
+            "poc": f"{url}/?q=<script>alert(1)</script>"
+        },
+        {
+            "type": "SQL Injection",
+            "url": url,
+            "description": "تم اكتشاف ثغرة SQLi في المعلمة `id`",
+            "poc": f"{url}/?id=1' OR '1'='1"
+        }
+    ]
