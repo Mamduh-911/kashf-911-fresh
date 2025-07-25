afe83e1d57a497f2eefbeaea88c2a1e629eac2ad
Mamduh-911 <132438162+Mamduh-911@users.noreply.github.com>
2025-07-25 23:51:36 +0300

    Update scan.py

--- scanner/scan.py
+++ scanner/scan.py
@@ -1,13 +1,10 @@
-import time
-
 def run_scan(url):
     print("[🔍] جاري فحص الرابط:", url)
-    time.sleep(2)  # انتظر 2 ثانية فقط
-    return [
-        {
-            'type': 'XSS',
-            'severity': 'High',
-            'description': 'تم اكتشاف إدخال قابل للتنفيذ في الحقول.',
-            'payload': '<script>alert(1)</script>'
-        }
-    ]
+    
+    # نتيجة تجريبية مؤقتة
+    return [{
+        'type': 'XSS',
+        'severity': 'High',
+        'description': 'تم اكتشاف إدخال قابل للتنفيذ في الحقول.',
+        'payload': '<script>alert(1)</script>'
+    }]
